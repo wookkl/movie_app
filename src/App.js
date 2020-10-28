b3f69da64f05352105b8616f31e432cd896846d0
wookkl <wjddnr315@gmail.com>
2020-10-28 18:34:23 +0900

    define css classes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
 import React from "react";
 import axios from "axios";
-import PropTypes from "prop-types";
-
+import Movie from "./Movie";
+import "./css/App.css";
 class App extends React.Component{
 
   state ={
 		isLoading:true,
 		movies:[],
-	};
+  };
+  
 	getMovies = async() =>{
-		return await axios.get("https://yts-proxy.now.sh/list_movies.json");
-	}
+		const {data:{data:{movies}}} = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+		this.setState({movies,isLoading:false});
+  }
+  
 	componentDidMount(){
-		const movies = this.getMovies();
-		this.setState({isLoading:false});
+		this.getMovies();
 	}
 	
   render(){
-		return <div>{this.state.isLoading?"Loading...":"We are ready"}</div>;
+	  const {isLoading,movies} = this.state;
+		return (
+    <section className="container">
+      {isLoading?(
+      <div className="loader">
+        <span className="loader__text">Loading...</span>
+      </div>
+      ) : (
+        <div className="movies">
+          {movies.map(movie => (
+            <Movie
+            key={movie.id}  
+            year = {movie.year}
+            title = {movie.title} 
+            summary = {movie.summary} 
+            poster = {movie.medium_cover_image}
+            />
+          ))}
+        </div>
+        )}
+    </section>);
 	}
 }
+
 export default App;
