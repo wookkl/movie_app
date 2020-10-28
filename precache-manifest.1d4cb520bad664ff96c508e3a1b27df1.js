dfa8df0e4b63f068e21f859c1ba105d23014a065
wookkl <wjddnr315@gmail.com>
2020-10-28 21:37:17 +0900

    Updates

---
+++ precache-manifest.1d4cb520bad664ff96c508e3a1b27df1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca24cd7b049ffae980ab280a5628e68c",
+    "url": "/wookkl.github.io/movie_app/index.html"
+  },
+  {
+    "revision": "1cf62082c3873598c5eb",
+    "url": "/wookkl.github.io/movie_app/static/css/main.7b83aa9c.chunk.css"
+  },
+  {
+    "revision": "39f75bacc7e29a4ac856",
+    "url": "/wookkl.github.io/movie_app/static/js/2.e533ec29.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/wookkl.github.io/movie_app/static/js/2.e533ec29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cf62082c3873598c5eb",
+    "url": "/wookkl.github.io/movie_app/static/js/main.e716c3e8.chunk.js"
+  },
+  {
+    "revision": "b6f50fa4b12d20808bea",
+    "url": "/wookkl.github.io/movie_app/static/js/runtime-main.c1ce7056.js"
+  }
+]);
